05bcb32142afbf01fd3ea89273067c1fb53ad0a0
diac <nikolai.gladkikh@gmail.com>
2023-02-17 18:45:06 +0300

    [#184504075]: Добавить модель SynthesisPrice

---
+++ domain/src/main/java/com/diac/oligos/domain/model/SynthesisPrice.java
@@ -0,0 +1,42 @@
+package com.diac.oligos.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Модель данных "Цена синтеза"
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SynthesisPrice {
+
+    /**
+     * Идентификатор цены синтеза
+     */
+    @EqualsAndHashCode.Include
+    private int id;
+
+    /**
+     * Значение цены
+     */
+    private int amount;
+
+    /**
+     * Синтез
+     */
+    private Synthesis synthesis;
+
+    /**
+     * Масштаб
+     */
+    private Scale scale;
+
+    /**
+     * Прейскурант
+     */
+    private PriceSchedule priceSchedule;
+}
