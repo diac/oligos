495d38e7e5d997899bbe1b3dd9c23f497499a874
diac <nikolai.gladkikh@gmail.com>
2023-02-17 18:14:35 +0300

    [#184503987]: Добавить модель Formulation

---
+++ domain/src/main/java/com/diac/oligos/domain/model/Formulation.java
@@ -0,0 +1,27 @@
+package com.diac.oligos.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * Модель данных "Тип препарата"
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Formulation {
+
+    /**
+     * Идентификатор типа препарата
+     */
+    @EqualsAndHashCode.Include
+    private int id;
+
+    /**
+     * Наименование типа препарата
+     */
+    private String name;
+}
