88404d6e0260ae243f579c7c0f35e939a0f604ff
diac <nikolai.gladkikh@gmail.com>
2023-02-21 16:00:58 +0300

    [#184528264]: Convert Synthesis to a JPA entity

--- domain/src/main/java/com/diac/oligos/domain/model/Synthesis.java
+++ domain/src/main/java/com/diac/oligos/domain/model/Synthesis.java
@@ -1,6 +1,7 @@
 package com.diac.oligos.domain.model;
 
 import com.diac.oligos.domain.enumeration.BaseType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,6 +10,8 @@ import lombok.NoArgsConstructor;
 /**
  * Модель данных "Синтез"
  */
+@Entity
+@Table(name = "synthesis")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -29,6 +32,8 @@ public class Synthesis {
     /**
      * Тип основания
      */
+    @Column(name = "base_type")
+    @Enumerated(EnumType.STRING)
     private BaseType baseType;
 
     /**
