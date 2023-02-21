ff728355678331af0d94dd39ace9b7dbc2488a90
diac <nikolai.gladkikh@gmail.com>
2023-02-21 16:11:30 +0300

    [#184517626]: Add PurificationRepository

---
+++ knowledgebase/src/main/java/com/diac/oligos/knowledgebase/repository/PurificationRepository.java
@@ -0,0 +1,23 @@
+package com.diac.oligos.knowledgebase.repository;
+
+import com.diac.oligos.domain.model.Formulation;
+import com.diac.oligos.domain.model.Purification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Репозиторий для хранения объектов Purification (Тип очистки)
+ */
+@Repository
+public interface PurificationRepository extends JpaRepository<Purification, Integer> {
+
+    /**
+     * Найти тип очистки по значению артикула
+     *
+     * @param sku Значение артикула
+     * @return Optional с найденным типом очистки. Пустой Optional, если ничего не найдено
+     */
+    Optional<Formulation> findBySku(String sku);
+}
