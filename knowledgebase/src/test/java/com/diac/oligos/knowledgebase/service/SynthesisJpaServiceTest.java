9b4a6e6cdf765932dfd6598331a8ddffb61a9065
diac <nikolai.gladkikh@gmail.com>
2023-02-22 21:10:56 +0300

    [#184517656]: Add test cases for exceptions to SynthesisJpaServiceTest

--- knowledgebase/src/test/java/com/diac/oligos/knowledgebase/service/SynthesisJpaServiceTest.java
+++ knowledgebase/src/test/java/com/diac/oligos/knowledgebase/service/SynthesisJpaServiceTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ContextConfiguration(classes = {
@@ -53,6 +55,26 @@ public class SynthesisJpaServiceTest {
     }
 
     @Test
+    public void whenAddWithNullFieldsThenThrowException() {
+        assertThrows(
+                DataIntegrityViolationException.class,
+                () -> synthesisService.add(buildSynthesis(null, null))
+        );
+    }
+
+    @Test
+    public void whenAddDuplicateThenThrowException() {
+        String value = String.valueOf(System.currentTimeMillis());
+        Synthesis synthesis = buildSynthesis(value, BaseType.DNA);
+        Synthesis duplicateSynthesis = buildSynthesis(value, BaseType.DNA);
+        synthesisService.add(synthesis);
+        assertThrows(
+                DataIntegrityViolationException.class,
+                () -> synthesisService.add(duplicateSynthesis)
+        );
+    }
+
+    @Test
     public void whenUpdate() {
         String value = String.valueOf(System.currentTimeMillis());
         Synthesis synthesis = synthesisService.add(buildSynthesis(value, BaseType.DNA));
@@ -64,6 +86,37 @@ public class SynthesisJpaServiceTest {
         assertThat(synthesis.getName()).isEqualTo(updatedSynthesis.getName());
         assertThat(synthesis.getSku()).isEqualTo(updatedSynthesis.getSku());
         assertThat(synthesis.getBaseType()).isEqualTo(updatedSynthesis.getBaseType());
+    }
+
+    @Test
+    public void whenUpdateWithNullValuesThenThrowException() {
+        Synthesis synthesis = synthesisService.add(buildSynthesis());
+        synthesis.setName(null);
+        synthesis.setSku(null);
+        synthesis.setBaseType(null);
+        assertThrows(
+                DataIntegrityViolationException.class,
+                () -> {
+                    synthesisService.update(synthesis);
+                    synthesisService.findAll();
+                }
+        );
+    }
+
+    @Test
+    public void whenUpdateDuplicateThenThrowException() {
+        String value = String.valueOf(System.currentTimeMillis());
+        Synthesis synthesis = synthesisService.add(buildSynthesis(value, BaseType.DNA));
+        Synthesis duplicateSynthesis = synthesisService.add(buildSynthesis(value + "_another", BaseType.DNA));
+        duplicateSynthesis.setName(synthesis.getName());
+        duplicateSynthesis.setSku(synthesis.getSku());
+        assertThrows(
+                DataIntegrityViolationException.class,
+                () -> {
+                    synthesisService.update(duplicateSynthesis);
+                    synthesisService.findAll();
+                }
+        );
     }
 
     @Test
