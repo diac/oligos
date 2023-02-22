b7e4cee1e0ac418b826a6047dce782d774abb6f4
diac <nikolai.gladkikh@gmail.com>
2023-02-22 10:03:06 +0300

    [#184517656]: Add ModificationService interface

---
+++ knowledgebase/src/main/java/com/diac/oligos/knowledgebase/service/ModificationService.java
@@ -0,0 +1,58 @@
+package com.diac.oligos.knowledgebase.service;
+
+import com.diac.oligos.domain.model.Modification;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сервис для работы с объектами модели Modification
+ */
+public interface ModificationService {
+
+    /**
+     * Найти все модификаторы
+     *
+     * @return Список модификаторов
+     */
+    List<Modification> findAll();
+
+    /**
+     * Найти модификатор по ID
+     *
+     * @param id Идентификатор модификатора
+     * @return Optional с найденным модификатором. Пустой Optional, если ничего не найдено
+     */
+    Optional<Modification> findById(int id);
+
+    /**
+     * Найти модификатор по артикулу
+     *
+     * @param sku Артикул модификатора
+     * @return Optional с найденным модификатором. Пустой Optional, если ничего не найдено
+     */
+    Optional<Modification> findBySku(String sku);
+
+    /**
+     * Добавить новый модификатор в систему
+     *
+     * @param modification Новый модификатор
+     * @return Сохраненный модификатор
+     */
+    Modification add(Modification modification);
+
+    /**
+     * Обновить данные модификатора в системе
+     *
+     * @param modification Модификатор, данные которого необходимо обновить
+     * @return Обновленный модификатор
+     */
+    Modification update(Modification modification);
+
+    /**
+     * Удалить модификатор из системы
+     *
+     * @param modification Модификатор, который необходимо удалить
+     */
+    void delete(Modification modification);
+}
