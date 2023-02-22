b3c5d067409e7c1573e16c25aa2b14aa50e2c854
diac <nikolai.gladkikh@gmail.com>
2023-02-22 19:38:23 +0300

    [#184517656]: Add SynthesisService interface

---
+++ knowledgebase/src/main/java/com/diac/oligos/knowledgebase/service/SynthesisService.java
@@ -0,0 +1,50 @@
+package com.diac.oligos.knowledgebase.service;
+
+import com.diac.oligos.domain.model.Synthesis;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Сервис для работы с объектами модели Synthesis
+ */
+public interface SynthesisService {
+
+    /**
+     * Найти все синтезы
+     *
+     * @return Список синтезов
+     */
+    List<Synthesis> findAll();
+
+    /**
+     * Найти синтез по ID
+     *
+     * @param id Идентификатор синтеза
+     * @return Optional с найденным синтезом. Пустой Optional, если ничего не найдено
+     */
+    Optional<Synthesis> findById(int id);
+
+    /**
+     * Добавить новый синтез в систему
+     *
+     * @param synthesis Новый синтез
+     * @return Сохраненный синтез
+     */
+    Synthesis add(Synthesis synthesis);
+
+    /**
+     * Обновить данные синтеза в системе
+     *
+     * @param synthesis Синтез, данные которого необходимо обновить
+     * @return Обновленный синтез
+     */
+    Synthesis update(Synthesis synthesis);
+
+    /**
+     * Удалить синтез из системы
+     *
+     * @param synthesis Синтез, который необходимо удалить
+     */
+    void delete(Synthesis synthesis);
+}
