5cd21f0bcd1b1e25509491358602a49845c64a7e
diac <nikolai.gladkikh@gmail.com>
2023-02-22 09:23:35 +0300

    [#184535915]: Add @Builder annotation to Shipping

--- domain/src/main/java/com/diac/oligos/domain/model/Shipping.java
+++ domain/src/main/java/com/diac/oligos/domain/model/Shipping.java
@@ -1,10 +1,7 @@
 package com.diac.oligos.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
@@ -17,6 +14,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
 public class Shipping {
 
     /**
