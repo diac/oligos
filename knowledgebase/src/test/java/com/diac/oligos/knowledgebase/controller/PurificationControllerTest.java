254e0ec539b7eea8fa955c0b6dc8360cef9d8f77
diac <nikolai.gladkikh@gmail.com>
2023-02-24 18:00:08 +0300

    [#184517749]: Add tests for PurificationController

---
+++ knowledgebase/src/test/java/com/diac/oligos/knowledgebase/controller/PurificationControllerTest.java
@@ -0,0 +1,133 @@
+package com.diac.oligos.knowledgebase.controller;
+
+import com.diac.oligos.domain.model.Purification;
+import com.diac.oligos.knowledgebase.service.PurificationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PurificationController.class)
+@AutoConfigureMockMvc
+public class PurificationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PurificationService purificationService;
+
+    private final ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
+    @Test
+    public void whenIndex() throws Exception {
+        mockMvc.perform(get("/purification"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenGetFound() throws Exception {
+        int id = 1;
+        Mockito.when(purificationService.findById(id)).thenReturn(Optional.of(Purification.builder().id(id).build()));
+        String requestUrl = String.format("/purification/%d", id);
+        mockMvc.perform(get(requestUrl))
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    public void whenGetNotFound() throws Exception {
+        int id = 1;
+        Mockito.when(purificationService.findById(id)).thenReturn(Optional.empty());
+        String requestUrl = String.format("/purification/%d", id);
+        mockMvc.perform(get(requestUrl))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void whenFindBySkuFound() throws Exception {
+        String sku = "qwerty";
+        Mockito.when(purificationService.findBySku(sku))
+                .thenReturn(
+                        Optional.of(Purification.builder()
+                                .id(1)
+                                .sku(sku)
+                                .build()
+                        )
+                );
+        String requestUrl = String.format("/purification/find_by_sku/%s", sku);
+        mockMvc.perform(get(requestUrl))
+                .andExpect(status().isFound());
+    }
+
+    @Test
+    public void whenFindBySkuNotFound() throws Exception {
+        String sku = "qwerty";
+        Mockito.when(purificationService.findBySku(sku)).thenReturn(Optional.empty());
+        String requestUrl = String.format("/purification/find_by_sku/%s", sku);
+        mockMvc.perform(get(requestUrl))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void whenPost() throws Exception {
+        int id = 1;
+        String value = String.valueOf(System.currentTimeMillis());
+        Purification newPurification = Purification.builder()
+                .name(value)
+                .sku(value)
+                .build();
+        Purification savedPurification = Purification.builder()
+                .id(id)
+                .name(value)
+                .sku(value)
+                .build();
+        String requestBody = objectWriter.writeValueAsString(newPurification);
+        String responseBody = objectWriter.writeValueAsString(savedPurification);
+        Mockito.when(purificationService.add(newPurification)).thenReturn(savedPurification);
+        mockMvc.perform(
+                        post("/purification")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isCreated())
+                .andExpect(content().json(responseBody));
+    }
+
+    @Test
+    public void whenPut() throws Exception {
+        int id = 1;
+        String value = String.valueOf(System.currentTimeMillis());
+        Purification purification = Purification.builder()
+                .id(id)
+                .name(value)
+                .sku(value)
+                .build();
+        String jsonValue = objectWriter.writeValueAsString(purification);
+        Mockito.when(purificationService.update(purification)).thenReturn(purification);
+        mockMvc.perform(
+                        put("/purification")
+                                .content(jsonValue)
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isOk())
+                .andExpect(content().json(jsonValue));
+    }
+
+    @Test
+    public void whenDelete() throws Exception {
+        int id = 1;
+        String requestUrl = String.format("/purification/%d", id);
+        mockMvc.perform(delete(requestUrl))
+                .andExpect(status().isOk());
+    }
+}
