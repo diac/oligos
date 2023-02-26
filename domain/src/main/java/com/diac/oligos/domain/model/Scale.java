513f1170059a69b1ab41c74a3d44c60424c2f0a6
diac <nikolai.gladkikh@gmail.com>
2023-02-26 15:34:42 +0300

    [#184556727]: Add validation rules to Scale

--- domain/src/main/java/com/diac/oligos/domain/model/Scale.java
+++ domain/src/main/java/com/diac/oligos/domain/model/Scale.java
@@ -1,6 +1,9 @@
 package com.diac.oligos.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -26,11 +29,15 @@ public class Scale {
     /**
      * Значение в наномолях
      */
+    @NotNull(message = "Scale nanomols value is required")
+    @Min(value = 1, message = "Scale nanomols value has to be greater or equal to 1")
     private int nanomols;
 
     /**
      * Отображаемое наименование
      */
     @Column(name = "display_name")
+    @NotNull(message = "Scale display name is required")
+    @NotBlank(message = "Scale display name cannot be blank")
     private String displayName;
 }
