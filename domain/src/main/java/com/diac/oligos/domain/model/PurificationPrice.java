a8800857c76bd5fbc6ab413652eb94335bdcd54e
diac <nikolai.gladkikh@gmail.com>
2023-03-01 13:49:41 +0300

    [#184556731]: Add data validation for PurificationPrice

--- domain/src/main/java/com/diac/oligos/domain/model/PurificationPrice.java
+++ domain/src/main/java/com/diac/oligos/domain/model/PurificationPrice.java
@@ -2,6 +2,9 @@ package com.diac.oligos.domain.model;
 
 import com.diac.oligos.domain.enumeration.BaseType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -27,12 +30,16 @@ public class PurificationPrice {
     /**
      * Значение цены
      */
+    @NotNull(message = "Price amount is required")
+    @Min(value = 0, message = "Price must be greater or equal to zero")
     private int amount;
 
     /**
      * Артикул типа очистки
      */
     @Column(name = "purification_sku")
+    @NotNull(message = "Purification SKU is required")
+    @NotBlank(message = "Purification SKU cannot be blank")
     private String purificationSku;
 
     /**
@@ -46,6 +53,8 @@ public class PurificationPrice {
      * Масштаб в наномолях
      */
     @Column(name = "scale_nanomols")
+    @NotNull(message = "Scale value is required")
+    @Min(value = 1, message = "Scale value must be greater or equal to 1")
     private int scaleNanomols;
 
     /**
@@ -53,5 +62,6 @@ public class PurificationPrice {
      */
     @ManyToOne
     @JoinColumn(name = "price_schedule_id")
+    @NotNull(message = "Associated Price Schedule is required")
     private PriceSchedule priceSchedule;
 }
