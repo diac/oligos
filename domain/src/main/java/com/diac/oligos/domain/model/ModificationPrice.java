059761bd5ef29441c2ba72b6c83ec7b14b313429
diac <nikolai.gladkikh@gmail.com>
2023-03-01 13:45:28 +0300

    [#184556731]: Add data validation for ModificationPrice

--- domain/src/main/java/com/diac/oligos/domain/model/ModificationPrice.java
+++ domain/src/main/java/com/diac/oligos/domain/model/ModificationPrice.java
@@ -1,6 +1,9 @@
 package com.diac.oligos.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -24,20 +27,26 @@ public class ModificationPrice {
     private int id;
 
     /**
-     * Значение цена
+     * Значение цены
      */
+    @NotNull(message = "Price amount is required")
+    @Min(value = 0, message = "Price must be greater or equal to zero")
     private int amount;
 
     /**
      * Артикул модификатора
      */
     @Column(name = "modification_sku")
+    @NotNull(message = "Modification SKU is required")
+    @NotBlank(message = "Modification SKU cannot be blank")
     private String modificationSku;
 
     /**
      * Масштаб в наномолях
      */
     @Column(name = "scale_nanomols")
+    @NotNull(message = "Scale value is required")
+    @Min(value = 1, message = "Scale value must be greater or equal to 1")
     private int scaleNanomols;
 
     /**
@@ -45,5 +54,6 @@ public class ModificationPrice {
      */
     @ManyToOne
     @JoinColumn(name = "price_schedule_id")
+    @NotNull(message = "Associated Price Schedule is required")
     private PriceSchedule priceSchedule;
 }
