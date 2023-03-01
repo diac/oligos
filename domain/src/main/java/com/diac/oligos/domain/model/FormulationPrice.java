eb3869bc2f159d4e6a282dace036a82b521ed678
diac <nikolai.gladkikh@gmail.com>
2023-03-01 13:43:18 +0300

    [#184556731]: Add data validation for FormulationPrice

--- domain/src/main/java/com/diac/oligos/domain/model/FormulationPrice.java
+++ domain/src/main/java/com/diac/oligos/domain/model/FormulationPrice.java
@@ -1,6 +1,9 @@
 package com.diac.oligos.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 /**
@@ -26,12 +29,16 @@ public class FormulationPrice {
     /**
      * Значение цены
      */
+    @NotNull(message = "Price amount is required")
+    @Min(value = 0, message = "Price must be greater or equal to zero")
     private int amount;
 
     /**
      * Артикул типа препарата
      */
     @Column(name = "formulation_sku")
+    @NotNull(message = "Formulation SKU is required")
+    @NotBlank(message = "Formulation SKU cannot be blank")
     private String formulationSku;
 
     /**
@@ -39,5 +46,6 @@ public class FormulationPrice {
      */
     @ManyToOne
     @JoinColumn(name = "price_schedule_id")
+    @NotNull(message = "Associated Price Schedule is required")
     private PriceSchedule priceSchedule;
 }
