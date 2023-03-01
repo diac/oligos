809208f09dd54fc3df764431c91d584a1fa229bc
diac <nikolai.gladkikh@gmail.com>
2023-03-01 14:35:48 +0300

    [#184517767]: Remove partial unique index from price_schedule DB table (not compatible with H2)

--- priceschedule/src/main/resources/db/scripts/001_ddl_create_table_price_schedule.sql
+++ priceschedule/src/main/resources/db/scripts/001_ddl_create_table_price_schedule.sql
@@ -5,14 +5,8 @@ CREATE TABLE price_schedule (
     effective BOOLEAN NOT NULL
 );
 
-CREATE UNIQUE INDEX u_price_schedule_allow_only_one_effective_to_be_true
-    ON price_schedule(effective) WHERE (effective = TRUE);
-
 COMMENT ON TABLE price_schedule IS 'Прейскурант';
 COMMENT ON COLUMN price_schedule.id IS 'Идентификатор прейскурранта';
 COMMENT ON COLUMN price_schedule.name IS 'Уникальное имя прейскуранта';
 COMMENT ON COLUMN price_schedule.created IS 'Дата и время создания прейскуранта';
 COMMENT ON COLUMN price_schedule.effective IS 'Признак действительности прейскуранта';
-
-COMMENT ON INDEX u_price_schedule_allow_only_one_effective_to_be_true
-    IS 'В таблице может быть только одна запись со значением effective, равным TRUE';
