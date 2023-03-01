7b15859a8caf95c8571d5c66585fc2b99be71094
diac <nikolai.gladkikh@gmail.com>
2023-03-01 16:35:09 +0300

    [#184517780]: Add Javadoc to PriceScheduleRepository

--- priceschedule/src/main/java/com/diac/oligos/priceschedule/repository/PriceScheduleRepository.java
+++ priceschedule/src/main/java/com/diac/oligos/priceschedule/repository/PriceScheduleRepository.java
@@ -4,6 +4,9 @@ import com.diac.oligos.domain.model.PriceSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Репозиторий для хранения объектов PriceSchedule (Прейскурант)
+ */
 @Repository
 public interface PriceScheduleRepository extends JpaRepository<PriceSchedule, Integer> {
 }
