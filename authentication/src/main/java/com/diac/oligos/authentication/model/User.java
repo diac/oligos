dd7d6c9d85c743437ed5dc298222394cf834a5f3
diac <nikolai.gladkikh@gmail.com>
2023-03-06 12:17:19 +0300

    [#184625935] Authentication Service: Add User model

---
+++ authentication/src/main/java/com/diac/oligos/authentication/model/User.java
@@ -0,0 +1,32 @@
+package com.diac.oligos.authentication.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @NotNull
+    @Field(name = "username")
+    private String username;
+
+    @Field(name = "password")
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    @Field(name = "isAdminUser")
+    private String isAdmin;
+}
