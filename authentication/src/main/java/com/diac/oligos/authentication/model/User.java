753c3cde295672fa1a2b77a0a310b751ce9e939f
diac <nikolai.gladkikh@gmail.com>
2023-03-09 11:58:58 +0300

    [#184639271] Authentication service: Convert User model to a JPA entity

--- authentication/src/main/java/com/diac/oligos/authentication/model/User.java
+++ authentication/src/main/java/com/diac/oligos/authentication/model/User.java
@@ -1,19 +1,42 @@
 package com.diac.oligos.authentication.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+/**
+ * Модель данных "Пользователь"
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
+    /**
+     * Идентификатор пользователя
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private int id;
 
+    /**
+     * Имя пользователя
+     */
+    @NotNull(message = "Username is required")
+    @NotBlank(message = "Username cannot be blank")
     private String username;
 
+    /**
+     * Пароль
+     */
+    @NotNull(message = "Password cannot be blank")
     private char[] password;
 }
