228fa26b2d3fa3a534b9a568e05bc372fa335957
diac <nikolai.gladkikh@gmail.com>
2023-03-09 11:12:43 +0300

    [#184639271] Authentication service: Remove the previous implementation

--- authentication/src/main/java/com/diac/oligos/authentication/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diac.oligos.authentication.repository;
-
-import com.diac.oligos.authentication.model.TokenEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokenRepository extends CrudRepository<TokenEntity, String> {
-}
--- authentication/src/main/java/com/diac/oligos/authentication/service/SequenceGeneratorService.java
+++
@@ -1,6 +0,0 @@
-package com.diac.oligos.authentication.service;
-
-public interface SequenceGeneratorService {
-
-    int generateSequence(String seqName);
-}
