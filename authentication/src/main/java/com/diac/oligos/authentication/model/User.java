a0e440cf8ac82c29211d2e53a707e56a40b66baa
diac <nikolai.gladkikh@gmail.com>
2023-03-09 11:23:38 +0300

    [#184517880] Authentication service: Add User model

---
+++ authentication/src/main/java/com/diac/oligos/authentication/model/User.java
@@ -0,0 +1,19 @@
+package com.diac.oligos.authentication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    private int id;
+
+    private String username;
+
+    private char[] password;
+}
