6e1dba054e3a0cf61422327d5441500d04c61374
diac <nikolai.gladkikh@gmail.com>
2023-03-09 12:27:50 +0300

    [#184639271] Authentication service: Add Javadoc to UserRepository

--- authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
@@ -6,8 +6,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Репозиторий для хранения объектов User (пользователь)
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    /**
+     * Найти пользователя по имени пользователя и паролю
+     *
+     * @param username Имя пользователя
+     * @param password Пароль
+     * @return Optional с найденным пользователем. Пустой Optional, если ничего не найдено
+     */
     Optional<User> findByUsernameAndPassword(String username, String password);
 }
