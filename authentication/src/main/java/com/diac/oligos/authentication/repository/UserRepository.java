47ed4826c34dc2b9cbf28a62eb3ab01f3289299c
diac <nikolai.gladkikh@gmail.com>
2023-03-09 12:15:59 +0300

    [#184639271] Authentication service: Correct data types in UserRepository

--- authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Integer, User> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUserNameAndPassword(String username, String password);
 }
