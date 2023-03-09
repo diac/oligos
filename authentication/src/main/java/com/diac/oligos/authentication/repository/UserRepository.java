839f657204d5ae972ca7d858bb315260f23ffae6
diac <nikolai.gladkikh@gmail.com>
2023-03-09 12:26:26 +0300

    [#184639271] Authentication service: UserRepository#findByUsernameAndPassword must return Optional<User>

--- authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.diac.oligos.authentication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    User findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
