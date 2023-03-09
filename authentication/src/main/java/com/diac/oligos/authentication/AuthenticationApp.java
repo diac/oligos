454ab04fe8e153360b3f7ed6430b3e2a81829c7d
diac <nikolai.gladkikh@gmail.com>
2023-03-09 11:26:50 +0300

    [#184639271] Authentication service: Rename the main class

--- authentication/src/main/java/com/diac/oligos/authentication/AuthenticationApp.java
+++ authentication/src/main/java/com/diac/oligos/authentication/AuthenticationApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthorizationApplication {
+public class AuthenticationApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthorizationApplication.class, args);
+        SpringApplication.run(AuthenticationApp.class, args);
     }
 }
