2be1cd6f405d541459e50deb545bc04f32a7f710
diac <nikolai.gladkikh@gmail.com>
2023-03-09 19:24:12 +0300

    [#184639271] Authentication service: Add ErrorDto

---
+++ authentication/src/main/java/com/diac/oligos/authentication/dto/ErrorDto.java
@@ -0,0 +1,11 @@
+package com.diac.oligos.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDto {
+
+    private String message;
+}
