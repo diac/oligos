355258cfdce3857f1f94b0dfad0455cbdcfeb2db
diac <nikolai.gladkikh@gmail.com>
2023-03-09 15:36:55 +0300

    [#184639271] Authentication service: Fix configuration values

--- authentication/src/main/java/com/diac/oligos/authentication/config/EncryptionConfig.java
+++ authentication/src/main/java/com/diac/oligos/authentication/config/EncryptionConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class EncryptionConfig {
 
-    @Value("bCryptEncoderStrength")
+    @Value("${security.b-crypt-encoder-strength}")
     private int bCryptEncoderStrength;
 
     @Bean
