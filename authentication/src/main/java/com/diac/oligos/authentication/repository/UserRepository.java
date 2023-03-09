89da7980de0b6f1d2845ce0a30b735165d885b48
diac <nikolai.gladkikh@gmail.com>
2023-03-09 12:07:14 +0300

    [#184639271] Authentication service: Add UserRepository

---
+++ authentication/src/main/java/com/diac/oligos/authentication/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.diac.oligos.authentication.repository;
+
+import com.diac.oligos.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Integer, User> {
+
+    User findByUserNameAndPassword(String username, String password);
+}
