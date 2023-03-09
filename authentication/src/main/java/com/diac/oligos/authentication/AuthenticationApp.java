d72a562667ce0a9567afd0e56d48b9634927575c
diac <nikolai.gladkikh@gmail.com>
2023-03-09 11:47:05 +0300

    [#184639271] Authentication service: Main app class must extend SpringBootServletInitializer

--- authentication/src/main/java/com/diac/oligos/authentication/AuthenticationApp.java
+++ authentication/src/main/java/com/diac/oligos/authentication/AuthenticationApp.java
@@ -2,9 +2,16 @@ package com.diac.oligos.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AuthenticationApp {
+public class AuthenticationApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AuthenticationApp.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApp.class, args);
